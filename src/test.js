3f0dd013a2fae6cbb1d42f08558923912694cb20
Kanke <kanke.ishaku@netbuilder.com>
2015-10-11 15:07:41 +0100

    Nashorn Example

---
+++ src/test.js
@@ -0,0 +1,9 @@
+var message = function(name) {
+    print('Hi' + name  );
+    return "greetings from inside javascript function";
+};
+
+//var NashornExample = Java.type('NashornExample');
+//
+//var result = MyJavaClass.message('Kanke');
+//print(result);
