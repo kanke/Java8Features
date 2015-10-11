952cd726e18f7823fac2bd88bb2ae16c17884e1f
Kanke <kanke.ishaku@netbuilder.com>
2015-10-11 11:40:23 +0100

    lambda anonymous class completed

--- src/main/java/Lambda/FileFilterAnonymous.java
+++ src/main/java/Lambda/FileFilterAnonymous.java
@@ -1,7 +1,39 @@
-package lambda.service;
+package lambda;
+
+import java.io.File;
+import java.io.FileFilter;
 
 /**
  * Created by kanke on 11/10/2015.
  */
 public class FileFilterAnonymous {
+    public static void main(String[] args) {
+
+        //TODO Replace with Lambda
+        FileFilter fileFilter = new FileFilter() {
+
+            @Override
+            public boolean accept(File pathname) {
+                return pathname.getName().endsWith(".html");
+            }
+        };
+
+
+        //change location to your location where you have any HTML file
+        File folder = new File("/Users/kanke/Desktop");
+        File[] listOfFiles = folder.listFiles(fileFilter);
+
+
+        for (File file : listOfFiles) {
+            System.out.println(file);
+        }
+
+    }
+
+    /**
+     * TO be deleted : For coaches to understand, Take parameter name, add lambda syntax and then return statement = lambda expression
+     */
+    // FileFilter fileFilter = (pathname) -> pathname.getName().endsWith(".html");
+
+
 }
