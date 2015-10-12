0044d8ac13880fb5c60e502ef81efc80fd48a08c
Kanke <kanke.ishaku@netbuilder.com>
2015-10-12 11:57:16 +0100

    date and time api

--- src/test.js
+++ src/test.js
@@ -2,8 +2,3 @@ var message = function(name) {
     print('Hi' + name  );
     return "greetings from inside javascript function";
 };
-
-//var NashornExample = Java.type('NashornExample');
-//
-//var result = MyJavaClass.message('Kanke');
-//print(result);
